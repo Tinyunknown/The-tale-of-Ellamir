0b7250163317da97e6c3763eef070000e30d506a
Tinyunknown156 <seanbrownlee2006@gmail.com>
2022-10-06 19:48:04 -0400

    added warning

---
+++ theTaleOfEllamir.py
@@ -0,0 +1,9 @@
+import time
+import random
+import os
+
+print("Created by Ligig games")
+print("")
+print("Important note: do not move the .exe file out of the folder it came in, as it will make the game unplayable.")
+print("You can make a shortcut to acess in quickly!")
+
