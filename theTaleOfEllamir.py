219dd13bac7eafe81eb82a4eca384e300a2e8917
tinyunknown156 <seanbrownlee2006@gmail.com>
2022-10-28 15:08:38 -0400

    added player variables and started on title screen functions

--- theTaleOfEllamir.py
+++ theTaleOfEllamir.py
@@ -2,10 +2,23 @@ import time
 import random
 import os
 
+//variables
+global playerHp
+global playerMoney
+global playerInvintory
+
+playerHp = 100
+playerMoney = 100
+playerInvintory = ["Basic sword", "Leather armour"]
+
 print("Created by Ligig games")
 print("")
 print("Important note: do not move the .exe file out of the folder it came in, as it will make the game unplayable.")
 print("You can make a shortcut to acess in quickly!")
+
+//functions
+
+// title screen look and options
 
 def titleScreen():
 	print("+=======================================================+")
@@ -19,7 +32,7 @@ def titleScreen():
 	print("|                                                       |")
 	print("|                                                       |")
 	print("|                                                       |")
-	print("|                                                       |")
+	print("|        Start           load            exit           |")
 	print("|                                                       |")
 	print("|                                                       |")
 	print("|                                                       |")
@@ -28,3 +41,9 @@ def titleScreen():
 	print("|                                                       |")
 	print("|                                                       |")
 	print("+=======================================================+")
+
+def titleScreenDecision():
+	answer = input()
+
+	if answer.lower().strip() == 'exit':
+		print("See you next time!")
