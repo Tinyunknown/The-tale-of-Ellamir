d9768e40a736189c43afb8c0fd7b7f2cb35a78a1
tinyunknown156 <seanbrownlee2006@gmail.com>
2022-11-04 09:02:50 -0400

    finished title screen functionality except for loading. Started on the intro

--- theTaleOfEllamir.py
+++ theTaleOfEllamir.py
@@ -11,10 +11,13 @@ playerHp = 100
 playerMoney = 100
 playerInvintory = ["Basic sword", "Leather armour"]
 
+//starting text and warning
+
 print("Created by Ligig games")
 print("")
 print("Important note: do not move the .exe file out of the folder it came in, as it will make the game unplayable.")
 print("You can make a shortcut to acess in quickly!")
+print("Loading game...")
 
 //functions
 
@@ -47,3 +50,16 @@ def titleScreenDecision():
 
 	if answer.lower().strip() == 'exit':
 		print("See you next time!")
+
+	elif answer.lower().strip() == 'load':
+		print("Sorry, I have not made that feature yet...")
+		titleScreenDecision()
+
+	elif answer.lower().strip() == 'start':
+		print("Let the game begin...")
+		intro()
+
+// start of the game text
+
+def intro():
+	print("")
